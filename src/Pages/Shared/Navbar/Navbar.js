5b86fd90569c9330aceb3dbdb66c57c1301a1824
maruf <dev.maruf112@gmail.com>
2022-11-24 03:56:04 +0600

    navbar options changed

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const menuItems = (
+    <React.Fragment>
+      <li>
+        <Link>Home</Link>
+      </li>
+    </React.Fragment>
+  );
+
   return (
     <div>
       <div className="navbar bg-base-100">
@@ -27,44 +35,13 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <Link>Home</Link>
-              </li>
+              {menuItems}
             </ul>
           </div>
           <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
-            <li>
-              <Link>Item 1</Link>
-            </li>
-            <li tabIndex={0}>
-              <Link>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </Link>
-              <ul className="p-2">
-                <li>
-                  <Link>Submenu 1</Link>
-                </li>
-                <li>
-                  <Link>Submenu 2</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link>Item 3</Link>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal p-0">{menuItems}</ul>
         </div>
         <div className="navbar-end">
           <Link className="btn">Get started</Link>
