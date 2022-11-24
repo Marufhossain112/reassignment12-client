6f6c4b0a253de438292dce7afd738b619fd38314
maruf <dev.maruf112@gmail.com>
2022-11-24 08:03:43 +0600

    routes set

---
+++ src/HomePage/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCategories from "../ProductCategories/ProductCategories";
+
+const Home = () => {
+  return (
+    <div>
+      <ProductCategories></ProductCategories>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/HomePage/ProductCategories/ProductCategories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductCategories = () => {
+    return (
+        <div>
+            <h2>This is Product categories.</h2>
+        </div>
+    );
+};
+
+export default ProductCategories;
