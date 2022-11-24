a976cdb795bfeb3b6f96031540a0b0e1d6497f8b
maruf <dev.maruf112@gmail.com>
2022-11-24 08:22:22 +0600

    load bikes data from database to client side

--- src/HomePage/ProductCategories/ProductCategories.js
+++ src/HomePage/ProductCategories/ProductCategories.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ProductCategories = () => {
-    return (
-        <div>
-            <h2>This is Product categories.</h2>
-        </div>
-    );
+  const [bikesData, setBikesData] = useState("");
+  //   setBikesData("");
+  useEffect(() => {
+    fetch("http://localhost:5000/allbikes")
+      .then((res) => res.json())
+      .then((data) => {
+        setBikesData(data);
+        // console.log(data);
+      });
+  }, []);
+  console.log(bikesData);
+
+  return (
+    <div>
+      <h2>Choose Your Brand</h2>
+      <div></div>
+    </div>
+  );
 };
 
 export default ProductCategories;
