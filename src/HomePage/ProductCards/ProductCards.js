4a139ec0b1196ffc5f8f75713d48fb8f631411a3
maruf <dev.maruf112@gmail.com>
2022-11-25 06:50:40 +0600

    firebase set up

--- src/HomePage/ProductCards/ProductCards.js
+++ src/HomePage/ProductCards/ProductCards.js
@@ -8,7 +8,12 @@ const ProductCards = ({ bike }) => {
     <div className="flex justify-center">
       <div className="rounded-lg shadow-lg bg-white max-w-sm">
         <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-          <img className="rounded-t-lg" src={pic} alt="" />
+          <img
+            style={{ height: "290px", width: "330px" }}
+            className="rounded-t-lg"
+            src={pic}
+            alt=""
+          />
         </a>
         <div className="p-6">
           <h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
@@ -27,7 +32,7 @@ const ProductCards = ({ bike }) => {
             type="button"
             className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
-            Button
+            Book Now
           </button>
         </div>
       </div>
